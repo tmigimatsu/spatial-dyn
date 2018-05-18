9ba51c31529d6fdd1ac7fbc28f4ba3a6f4d35f49
Toki Migimatsu <toki.migimatsu@gmail.com>
2018-05-18 02:04:37 -0700

    Vectorwise cross product

--- src/Eigen/SpatialTransform.h
+++ src/Eigen/SpatialTransform.h
@@ -11,12 +11,8 @@ template<typename OtherDerived>
 EIGEN_DEVICE_FUNC inline typename SpatialMotionBase<OtherDerived>::PlainObject
 operator*(const SpatialMotionBase<OtherDerived>& other) const {
   typename SpatialMotionBase<OtherDerived>::PlainObject result = other;
-  Eigen::Matrix3d T_x;
-  T_x <<  0,                      -this->translation()(2),  this->translation()(1),
-          this->translation()(2),  0,                      -this->translation()(0),
-         -this->translation()(1),  this->translation()(0),  0;
-  result.template topRows<3>() = this->linear() *
-      (result.matrix().template topRows<3>() - T_x * result.matrix().template bottomRows<3>());
+  result.template topRows<3>() = this->linear() * (result.matrix().template topRows<3>() +
+      result.matrix().template bottomRows<3>().colwise().cross(this->translation()));
   result.template bottomRows<3>() = this->linear() * result.matrix().template bottomRows<3>();
   return result;
 }
@@ -25,12 +21,8 @@ template<typename OtherDerived>
 EIGEN_DEVICE_FUNC inline typename SpatialForceBase<OtherDerived>::PlainObject
 operator*(const SpatialForceBase<OtherDerived>& other) const {
   typename SpatialForceBase<OtherDerived>::PlainObject result = other;
-  Eigen::Matrix3d T_x;
-  T_x <<  0,                      -this->translation()(2),  this->translation()(1),
-          this->translation()(2),  0,                      -this->translation()(0),
-         -this->translation()(1),  this->translation()(0),  0;
-  result.template bottomRows<3>() = this->linear() *
-      (result.matrix().template bottomRows<3>() - T_x * result.matrix().template topRows<3>());
+  result.template bottomRows<3>() = this->linear() * (result.matrix().template bottomRows<3>() +
+      result.matrix().template topRows<3>().colwise().cross(this->translation()));
   result.template topRows<3>() = this->linear() * result.matrix().template topRows<3>();
   return result;
 }
@@ -39,18 +31,13 @@ template<typename OtherScalar>
 EIGEN_DEVICE_FUNC inline SpatialInertia<OtherScalar>
 operator*(const SpatialInertia<OtherScalar>& other) const {
   SpatialInertia<OtherScalar> result;
-  Eigen::Matrix3d T_x;
-  T_x <<  0,                      -this->translation()(2),  this->translation()(1),
-          this->translation()(2),  0,                      -this->translation()(0),
-         -this->translation()(1),  this->translation()(0),  0;
   result.template leftCols<3>() = other.template leftCols<3>() * this->linear();
-  result.template rightCols<3>() = other.template rightCols<3>() * this->linear() -
-                                   result.template leftCols<3>() * T_x;
-  T_x = this->linear() * T_x;
-  result.template bottomLeftCorner<3,3>() = this->linear() * result.template bottomLeftCorner<3,3>() -
-                                    T_x * result.template topLeftCorner<3,3>();
-  result.template bottomRightCorner<3,3>() = this->linear() * result.template bottomRightCorner<3,3>() -
-                                     T_x * result.template topRightCorner<3,3>();
+  result.template rightCols<3>() = other.template rightCols<3>() * this->linear() +
+      result.template leftCols<3>().rowwise().cross(this->translation());
+  result.template bottomLeftCorner<3,3>() = this->linear() * (result.template bottomLeftCorner<3,3>() +
+      result.template topLeftCorner<3,3>().colwise().cross(this->translation()));
+  result.template bottomRightCorner<3,3>() = this->linear() * (result.template bottomRightCorner<3,3>() +
+      result.template topRightCorner<3,3>().colwise().cross(this->translation()));
   result.template topLeftCorner<3,3>() = this->linear() * result.template topLeftCorner<3,3>();
   result.template topRightCorner<3,3>() = this->linear() * result.template topRightCorner<3,3>();
   return result;
