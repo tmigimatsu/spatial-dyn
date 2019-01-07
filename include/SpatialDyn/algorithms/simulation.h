2eb974ee3d80aec5a5f1617940309ee0f4ef951a
Toki Migimatsu <toki.migimatsu@gmail.com>
2019-01-06 20:56:21 -0800

    Add comments for simulation

--- include/SpatialDyn/algorithms/simulation.h
+++ include/SpatialDyn/algorithms/simulation.h
@@ -18,6 +18,34 @@
 
 namespace SpatialDyn {
 
+/**
+ * @defgroup cpp_simulation Simulation
+ * @ingroup cpp
+ *
+ * C++ implementation of SpatialDyn simulation utilities.
+ *
+ * @see Python: \ref py_simulation
+ * @{
+ */
+
+/**
+ * Apply the commanded joint torques and integrate by the given timestep to get
+ * the robot's next state.
+ *
+ * Computes the robot's acceleration with forward dynamics and integrates the
+ * resulting acceleration along with robot's velocity by one timestep to get the
+ * robot's next position and velocity. The forward dynamics algorithm and
+ * integration method can be specified in the options.
+ *
+ * @param ab ArticulatedBody.
+ * @param tau Commanded joint torques.
+ * @param dt Integration timestep.
+ * @param f_external Map of (index, force) pairs where the force is the sum of
+ *                   all external spatial forces (represented in the world
+ *                   frame) applied to the associated rigid body index.
+ * @param options IntegrationOptions.
+ * @see Python: spatialdyn.integrate()
+ */
 void Integrate(ArticulatedBody &ab, const Eigen::VectorXd& tau, double dt,
                const std::map<int, SpatialForced>& f_external = {},
                const IntegrationOptions& options = {});
